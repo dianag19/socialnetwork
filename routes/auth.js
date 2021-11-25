0a16a54d8285cb59916a4bdb58ab0e0d56f52ab0
Diana Gomez <diana.gomez.caicedo@correounivalle.edu.co>
2021-11-25 17:53:26 -0500

    login api

--- routes/auth.js
+++ routes/auth.js
@@ -23,7 +23,7 @@ router.post("/register",async (req,res)=>{
         const user = await newUser.save();
         res.status(200).json(user);
     }catch(err){
-        console.log(err)
+        res.status(500).json(err)
     }
 });
 
@@ -32,8 +32,13 @@ router.post("/login", async (req,res)=>{
     try{
         const user = await User.findOne({email:req.body.email});
         !user && res.status(404).send("User not found");
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password)
+        !validPassword && res.status(400).json("wrong password");
+
+        res.status(200).json(user);
     } catch(err){
-        console.log(err)
+        res.status(500).json(err)
     }
     
 })
