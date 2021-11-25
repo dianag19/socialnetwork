86290c32498e7a3c8bc99f66b0f38c6506d91f55
Diana Gomez <diana.gomez.caicedo@correounivalle.edu.co>
2021-11-25 10:43:53 -0500

    env and index

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+dotenv.config();
+
+//db connection
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true}, ()=>{
+    console.log("Connected to MongoDB");
+});//set mongo url from env
+
+//middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+/*app.get("/", (req,res)=>{
+    res.send("Welcome to homepage");
+});
+
+app.get("/users", (req,res)=>{
+    res.send("Welcome to user page");
+})*/
+
+app.listen(8800, ()=>{
+    console.log("Backend server is runing!")
+})
