d03f61fab3af9eced8d6654d5a3d2fe39813d560
Diana Gomez <diana.gomez.caicedo@correounivalle.edu.co>
2021-11-25 19:37:49 -0500

    create post and model post

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts")
 dotenv.config();
 
 //db connection
@@ -19,6 +20,7 @@ app.use(helmet());
 app.use(morgan("common"));
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 /*app.get("/", (req,res)=>{
     res.send("Welcome to homepage");
 });
