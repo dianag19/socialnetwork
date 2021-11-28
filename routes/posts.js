ff214c643430de2609b47fe2309c25a526be3ea5
Diana Gomez <diana.gomez.caicedo@correounivalle.edu.co>
2021-11-28 18:51:06 -0500

    fix posts

--- routes/posts.js
+++ routes/posts.js
@@ -71,10 +71,13 @@ router.get("/:id", async (req,res)=>{
 router.post("/timeline/all", async (req, res) => {
     try {
       const currentUser = await User.findById(req.body.userId);
-      const userPosts = await Post.findAndModify({ userId: currentUser._id }).sort({createdAt : -1});
+      const options ={
+          "sort": "createdAt"
+      }
+      const userPosts = await Post.find({ userId: currentUser._id })
       const friendPosts = await Promise.all(
         currentUser.followings.map((friendId) => {
-          return Post.find({ userId: friendId }).sort({createdAt : -1});
+          return Post.find({ userId: friendId })
         })
       );
       res.json(userPosts.concat(...friendPosts))
